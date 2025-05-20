5628d8ae8a3d93dc96fa0014b5bb7a7b7f441f5b
carzarrei <carloszarzuelar@gmail.com>
2025-05-20 13:42:21 +0200

    fix: Eliminar imports sin usar

--- frontend/src/components/Groups/GroupRequestForm.js
+++ frontend/src/components/Groups/GroupRequestForm.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import api from '../../api';
-import { groupRequests, login, requestsList } from '../../routeNames';
+import { login } from '../../routeNames';
 import '../../styles/Requests/requestForm.css';
 import { useParams } from 'react-router-dom';
 
